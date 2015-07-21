963c26276427bec2ae6ff6bba353d875a6007cde
mlw125 <mlw125@txstate.edu>
2015-07-21 10:57:16 -0500

    Changed the names to be 'correct'.

---
+++ PointOfSale.cpp
@@ -0,0 +1,187 @@
+// Matthew Williams 7/16/2015, modified 7/19/2015
+// David Bumgarner  Modified and expanded 7/17/15
+#include "PointOfSale.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+// Login Functions
+Login::Login()
+{
+	// I don't think this does anything currently
+	fstream loginFile;
+	string nameData = "";
+	string passwordData = "";
+	loginFile.open("Logins.txt");
+
+	while (loginFile >> nameData >> passwordData)
+	{
+		nameList.push_back(nameData);
+		passwordList.push_back(passwordData);
+	}
+	loginFile.close();
+} // end Login Constructor
+
+string Login::getEmployee()
+{
+	return userName;
+} // end getEmployee()
+
+string Login::getPassword()
+{
+	return password;
+} // end getPassword()
+
+void Login::setEmployee(string user)
+{
+     userName = user;
+} // end setEmployee()
+
+void Login::setPassword(string pass)
+{
+	password = pass;
+} // end setPassword()
+
+// User Functions
+// Manager Functions
+// Employee Prices
+
+// Register Functions
+
+// this function will return the price of the selected menu item
+double Register::itemPrice(int menuItem)
+{
+	Menu menu;
+	return 10.00;//menu.searchPrice(menuItem);
+} // end itemPrice
+
+// this function cumulates the total cost
+void Register::addToOrder(int menuNum)
+{
+	total += itemPrice(menuNum);
+	currentOrder.push_back(menuNum);
+} // end addToOrder
+
+// this function returns the total cost
+double Register::getTotal()
+{
+	return total;
+} // end getTotal
+
+// Menu Functions
+Menu::Menu()
+{	
+	// *Not reading*
+	ifstream MenuFile("Menu.txt");
+	//MenuFile.open("Menu.txt");
+
+	int numData = 0;
+	string nameData = "";
+	double priceData = 0.0;
+
+	while (MenuFile >> numData >> nameData >> priceData)
+	{
+		menuNumber.push_back(numData);
+		menuItem.push_back(nameData);
+		menuPrice.push_back(priceData);
+		numItems++;
+	} // end while
+	MenuFile.close();
+} // end Menu contructor
+
+bool Menu::addMenuItems(int num, string name, double price)
+{
+	// if the item number is not in the menu then add the new item to each vector
+	if (searchPosition(num) == -1)
+	{
+		menuItem.push_back(name);
+		menuPrice.push_back(price);
+		menuNumber.push_back(num);
+		numItems++;
+		return true;
+	} // end if
+	else
+		return false; // end else
+} // end addMenuItems()
+
+// This function searches through the Menu class vectors and will displat them in order.
+// This way we will not have to sort the vectors, but this can be changed. 
+void Menu::showMenuItems()
+{
+	// *Bad logic, not working right*
+	cout << endl;
+	for (int x = 0; x < numItems; x++)
+	{
+		cout << menuNumber[x] << ". " << menuItem[x] << " $" << menuPrice[x] << endl;
+	} // end for
+} // end showMenuItems()
+
+bool Menu::removeMenuItems(int num)
+{
+	int removePosition = searchPosition(num);
+	// if the item number is not in the menu then add the new item to each vector
+	if (removePosition != -1)
+	{
+		menuItem.erase(menuItem.begin() + removePosition);
+		menuPrice.erase(menuPrice.begin() + removePosition);
+		menuNumber.erase(menuNumber.begin() + removePosition);
+		numItems--;
+		return true;
+	} // end if
+	else
+		return false; // end else
+} // end removeMenuItems()
+
+// This function is used when getting totals for an order. Will return the price if that number is in the
+// menu or otherwise return a -1 to signal it was not found.
+double Menu::searchPrice(int menuNum)
+{
+	int getPricePosition = searchPosition(menuNum);
+	if (getPricePosition != -1)
+		return menuPrice[getPricePosition];
+	else
+		return getPricePosition;
+}
+
+// simply returns the size of the Menu vectors
+int Menu::getMenuSize()
+{
+	return menuNumber.size();
+} // end getMenuSize()
+
+int Menu::searchPosition(int num)
+{
+	for (int x = 0; x < numItems; x++)
+	{
+		if (menuNumber[x] == num)
+			return x;
+	} // end for
+	return -1;
+} // end searchPosition()
+
+void Menu::sortMenu()
+{
+	for (int x = 0; x < numItems; x++)
+	{
+		int minIndex = x;
+
+		for (int y = x; y < numItems; y++)
+			if (menuNumber[minIndex] > menuNumber[y])
+				minIndex = y; // end if
+		// end for
+
+		int tempNum = menuNumber[x];
+		menuNumber[x] = menuNumber[minIndex];
+		menuNumber[minIndex] = tempNum;
+
+		string tempItem = menuItem[x];
+		menuItem[x] = menuItem[minIndex];
+		menuItem[minIndex] = tempItem;
+
+		double tempPrice = menuPrice[x];
+		menuPrice[x] = menuPrice[minIndex];
+		menuPrice[minIndex] = tempPrice;
+	} // end for
+} // end sortMenu()
---
+++ PointOfSale.h
@@ -0,0 +1,94 @@
+// Matthew Williams 7/16/2015, modified 7/19/2015
+
+/*
+This is what I have so far. I have tried to explain why I did what I did. Feel free to make changes and suggest new things.
+*/
+
+#ifndef PointOfSale_H
+#define PointOfSale_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+// this will handle logon checks. I am not sure how we want to implement this, 
+// maybe a text document (temporarily?) that handles the employees and perhaps position. 
+class Login
+{
+      private:
+              string userName;
+              string password;
+			  vector<string> nameList;
+			  vector<string> passwordList;
+      public:
+		Login();
+        string getEmployee();
+        string getPassword();
+        void setEmployee(string);
+        void setPassword(string);
+}; // end Login
+
+// Not sure how to implement the User class and it's children yet if anyone could do so that would be great.
+class User
+{
+}; // end User
+
+class Employee : public User
+{
+}; // end Employee
+
+class Manager : public User
+{
+}; // end Manager
+
+// The "main" part of the program as I see it. Will get information from the Menu class
+// and display totals and change due.
+class Register
+{
+	private:
+		double total;
+		vector<int> currentOrder;
+		double itemPrice(int);
+	public:
+		void addToOrder(int);
+		void removeFromOrder();
+		void closeOrder();
+		void getOrder();
+		double getTotal();
+}; // end Register
+
+
+// the logging class will handle the storing of the transations.
+// I have left it alone until we decide how to implement it i.e. text vs database.
+class Logging
+{
+	private:
+	public:
+		void addTransaction();
+		void viewTransactions();
+}; // end Logging
+
+
+// As I saw this this will handle the items on the Menu and is able to be modified to
+// to handle menu changes.
+class Menu
+{
+	private:
+		// the idea is that each vector will hold information from the menu.
+		// This way we do not have to worry about arrays.
+		vector<int> menuNumber;
+		vector<string> menuItem;
+		vector<double> menuPrice;
+		int numItems;
+	public:
+		Menu();
+		bool addMenuItems(int, string, double);
+		void showMenuItems();
+		bool removeMenuItems(int);
+		double searchPrice(int);
+		int getMenuSize();
+		int searchPosition(int);
+		void sortMenu();
+}; // end Menu
+
+#endif
